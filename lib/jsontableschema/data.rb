58802bd472d5a0d9749eb0e3096685b86d604930
pezholio <pezholio@gmail.com>
2016-09-16 12:09:57 +0100

    Convert multiple rows

--- lib/jsontableschema/data.rb
+++ lib/jsontableschema/data.rb
@@ -3,21 +3,36 @@ module JsonTableSchema
 
     attr_reader :errors
 
+    def convert(rows, fail_fast = true)
+      @errors ||= []
+      rows.map! do |r|
+        begin
+          convert_row(r, fail_fast)
+        rescue MultipleInvalid, ConversionError => e
+          raise e if fail_fast == true
+          @errors << e if e.is_a?(ConversionError)
+        end
+      end
+      check_for_errors
+      rows
+    end
+
     def convert_row(row, fail_fast = true)
       @errors ||= []
       raise_header_error(row) if row.count != fields.count
       fields.each_with_index do |field,i|
         row[i] = convert_column(row[i], field, fail_fast)
       end
-      if @errors.count > 0
-        raise(JsonTableSchema::MultipleInvalid.new("There were errors parsing the row"))
-      else
-        row
-      end
+      check_for_errors
+      row
     end
 
     def raise_header_error(row)
       raise(JsonTableSchema::ConversionError.new("The number of items to convert (#{row.count}) does not match the number of headers in the schema (#{fields.count})"))
+    end
+
+    def check_for_errors
+      raise(JsonTableSchema::MultipleInvalid.new("There were errors parsing the data")) if @errors.count > 0
     end
 
     def convert_column(col, field, fail_fast)
--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -46,120 +46,133 @@ describe JsonTableSchema::Data do
 
   let(:schema) { JsonTableSchema::Schema.new(schema_hash) }
 
-  it 'converts a row' do
-    row = ['string', '10.0', '1', 'string', 'string']
-    expect(schema.convert_row(row)).to eq(['string', Float(10.0), 1, 'string', 'string'])
+  context 'convert_row' do
+
+    it 'converts a row' do
+      row = ['string', '10.0', '1', 'string', 'string']
+      expect(schema.convert_row(row)).to eq(['string', Float(10.0), 1, 'string', 'string'])
+    end
+
+    it 'converts a row with null values' do
+      row = ['string', '', '-', 'string', 'null']
+      expect(schema.convert_row(row)).to eq(['string', nil, nil, 'string', nil])
+    end
+
+    it 'raises an error for a row with too few items' do
+      row = ['string', '10.0', '1', 'string']
+      expect { schema.convert_row(row) }.to raise_error(
+        JsonTableSchema::ConversionError,
+        'The number of items to convert (4) does not match the number of headers in the schema (5)'
+      )
+    end
+
+    it 'raises an error for a row with too many items' do
+      row = ['string', '10.0', '1', 'string', 1, 2]
+      expect { schema.convert_row(row) }.to raise_error(
+        JsonTableSchema::ConversionError,
+        'The number of items to convert (6) does not match the number of headers in the schema (5)'
+      )
+    end
+
+    it 'raises an error if a column has the wrong type' do
+      row = ['string', 'notdecimal', '10.6', 'string', 'string']
+      expect { schema.convert_row(row) }.to raise_error(
+        JsonTableSchema::InvalidCast,
+        'notdecimal is not a number'
+      )
+    end
+
+    it 'raises multiple errors if fail_fast is set to false' do
+      row = ['string', 'notdecimal', '10.6', 'string', 'string']
+      expect { schema.convert_row(row, false) }.to raise_error(
+        JsonTableSchema::MultipleInvalid,
+        'There were errors parsing the data'
+      )
+      expect(schema.errors.count).to eq(2)
+    end
+
   end
 
-  it 'converts a row with null values' do
-    row = ['string', '', '-', 'string', 'null']
-    expect(schema.convert_row(row)).to eq(['string', nil, nil, 'string', nil])
-  end
+  context 'convert' do
 
-  it 'raises an error for a row with too few items' do
-    row = ['string', '10.0', '1', 'string']
-    expect { schema.convert_row(row) }.to raise_error(
-      JsonTableSchema::ConversionError,
-      'The number of items to convert (4) does not match the number of headers in the schema (5)'
-    )
-  end
+    it 'converts valid data' do
+      rows = [
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string']
+      ]
 
-  it 'raises an error for a row with too many items' do
-    row = ['string', '10.0', '1', 'string', 1, 2]
-    expect { schema.convert_row(row) }.to raise_error(
-      JsonTableSchema::ConversionError,
-      'The number of items to convert (6) does not match the number of headers in the schema (5)'
-    )
-  end
+      converted_rows = schema.convert(rows)
 
-  it 'raises an error if a column has the wrong type' do
-    row = ['string', 'notdecimal', '10.6', 'string', 'string']
-    expect { schema.convert_row(row) }.to raise_error(
-      JsonTableSchema::InvalidCast,
-      'notdecimal is not a number'
-    )
-  end
+      converted_rows.each do |row|
+        expect(row).to eq(['string', Float(10.0), 1, 'string', 'string'])
+      end
+    end
 
-  it 'raises multiple errors if fail_fast is set to false' do
-    row = ['string', 'notdecimal', '10.6', 'string', 'string']
-    expect { schema.convert_row(row, false) }.to raise_error(
-      JsonTableSchema::MultipleInvalid,
-      'There were errors parsing the row'
-    )
-    expect(schema.errors.count).to eq(2)
+    it 'raises the first error it comes to' do
+      rows = [
+        ['string', 'not', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', 'an', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', 'integer', 'string', 'string']
+      ]
+
+      expect { schema.convert(rows) }.to raise_error(
+        JsonTableSchema::InvalidCast,
+        'not is not a number'
+      )
+    end
+
+    it 'collects errors when fail_fast is set to false' do
+      rows = [
+        ['string', 'not', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', 'an', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', 'integer', 'string', 'string']
+      ]
+
+      expect { schema.convert(rows, false) }.to raise_error(
+        JsonTableSchema::MultipleInvalid,
+        'There were errors parsing the data'
+      )
+      expect(schema.errors.count).to eq(3)
+    end
+
+    it 'collects row length errors too' do
+      rows = [
+        ['string', '10.0', '1', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string', 1],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string']
+      ]
+
+      expect { schema.convert(rows, false) }.to raise_error(
+        JsonTableSchema::MultipleInvalid,
+        'There were errors parsing the data'
+      )
+      expect(schema.errors.count).to eq(2)
+    end
+
+    it 'fails on the first row length error when fail_fast is true' do
+      rows = [
+        ['string', '10.0', '1', 'string'],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string', 1],
+        ['string', '10.0', '1', 'string', 'string'],
+        ['string', '10.0', '1', 'string', 'string']
+      ]
+
+      expect { schema.convert(rows) }.to raise_error(
+        JsonTableSchema::ConversionError,
+        'The number of items to convert (4) does not match the number of headers in the schema (5)'
+      )
+    end
+
   end
 
 end
-
-
-#
-# def test_convert_rows(self):
-#     m = model.SchemaModel(self.schema)
-#     rows = m.convert([['string', '10.0', '1', 'string', 'string'],
-#                       ['string', '10.0', '1', 'string', 'string'],
-#                       ['string', '10.0', '1', 'string', 'string'],
-#                       ['string', '10.0', '1', 'string', 'string'],
-#                       ['string', '10.0', '1', 'string', 'string']])
-#     for row in rows:
-#         self.assertEqual(['string', Decimal(10.0), 1, 'string', 'string'],
-#                          row)
-#
-# def test_convert_rows_invalid_in_various_rows_fail_fast(self):
-#     m = model.SchemaModel(self.schema)
-#     self.assertRaises(
-#         exceptions.InvalidCastError,
-#         list,
-#         m.convert(
-#             [['string', 'not', '1', 'string', 'string'],
-#              ['string', '10.0', '1', 'string', 'string'],
-#              ['string', 'an', '1', 'string', 'string'],
-#              ['string', '10.0', '1', 'string', 'string'],
-#              ['string', '10.0', 'integer', 'string', 'string']],
-#             fail_fast=True)
-#     )
-#
-# def test_convert_rows_invalid_in_various_rows(self):
-#     m = model.SchemaModel(self.schema)
-#     with self.assertRaises(exceptions.MultipleInvalid) as cm:
-#         list(m.convert([['string', 'not', '1', 'string', 'string'],
-#                         ['string', '10.0', '1', 'string', 'string'],
-#                         ['string', 'an', '1', 'string', 'string'],
-#                         ['string', '10.0', '1', 'string', 'string'],
-#                         ['string', '10.0', 'integer', 'string', 'string']])
-#              )
-#         self.assertEquals(3, len(cm.errors))
-#
-# def test_convert_rows_invalid_varying_length_rows(self):
-#     m = model.SchemaModel(self.schema)
-#     with self.assertRaises(exceptions.MultipleInvalid) as cm:
-#         list(m.convert([['string', '10.0', '1', 'string'],
-#                         ['string', '10.0', '1', 'string', 'string'],
-#                         ['string', '10.0', '1', 'string', 'string', 1],
-#                         ['string', '10.0', '1', 'string', 'string'],
-#                         ['string', '10.0', '1', 'string', 'string']])
-#              )
-#         self.assertEquals(2, len(cm.errors))
-#
-# def test_convert_rows_invalid_varying_length_rows_fail_fast(self):
-#     m = model.SchemaModel(self.schema)
-#     self.assertRaises(
-#         exceptions.ConversionError,
-#         list,
-#         m.convert([['string', '10.0', '1', 'string'],
-#                    ['string', '10.0', '1', 'string', 'string'],
-#                    ['string', '10.0', '1', 'string', 'string', 1],
-#                    ['string', '10.0', '1', 'string', 'string'],
-#                    ['string', '10.0', '1', 'string', 'string']],
-#                   fail_fast=True)
-#     )
-
-
-
-
-
-
-# def test_invalid_jts_raises(self):
-#     source = os.path.join(self.data_dir, 'schema_invalid_empty.json')
-#
-#     self.assertRaises(exceptions.InvalidSchemaError,
-#                       model.SchemaModel, source)
