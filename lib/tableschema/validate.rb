2a8b3fd8c02f0a64c8c1e6b3d87fdedac5af3ad0
Georgiana Bere <bere.georgi@gmail.com>
2017-08-16 11:59:47 +0300

    Update Schema interface [#17]

--- lib/tableschema/validate.rb
+++ lib/tableschema/validate.rb
@@ -8,51 +8,53 @@ module TableSchema
       @validator ||= JSON.parse(File.read(filepath), symbolize_names: true)
     end
 
-    def valid?
-      validate
-      @errors.count == 0
-    end
-
     def validate
       @errors = Set.new(JSON::Validator.fully_validate(@validator, self))
       check_primary_keys
       check_foreign_keys
+      @errors.empty?
+    end
+
+    def validate!
+      validate
+      raise SchemaException.new(@errors.first) unless @errors.empty?
+      true
     end
 
     private
 
-      def check_primary_keys
-        return if self[:primaryKey].nil?
-        primary_keys.each { |pk| check_field_value(pk, 'primaryKey') }
-      end
+    def check_primary_keys
+      return if self[:primaryKey].nil?
+      primary_keys.each { |pk| check_field_value(pk, 'primaryKey') }
+    end
 
-      def check_foreign_keys
-        return if self[:foreignKeys].nil?
-        self[:foreignKeys].each do |key|
-          foreign_key_fields(key).each { |fk| check_field_value(fk, 'foreignKey.fields') }
-          if field_count_mismatch?(key)
-            add_error("A JSON Table Schema foreignKey.fields must contain the same number entries as foreignKey.reference.fields.")
-          end
+    def check_foreign_keys
+      return if self[:foreignKeys].nil?
+      self[:foreignKeys].each do |key|
+        foreign_key_fields(key).each { |fk| check_field_value(fk, 'foreignKey.fields') }
+        if field_count_mismatch?(key)
+          add_error("A TableSchema foreignKey.fields must contain the same number entries as foreignKey.reference.fields.")
         end
       end
+    end
 
-      def check_field_value(key, type)
-        if headers.select { |f| key == f }.count == 0
-          add_error("The JSON Table Schema #{type} value `#{key}` is not found in any of the schema's field names")
-        end
+    def check_field_value(key, type)
+      if headers.select { |f| key == f }.count == 0
+        add_error("The TableSchema #{type} value `#{key}` is not found in any of the schema's field names")
       end
+    end
 
-      def foreign_key_fields(key)
-        [key[:fields]].flatten
-      end
+    def foreign_key_fields(key)
+      [key[:fields]].flatten
+    end
 
-      def field_count_mismatch?(key)
-        key[:reference] && ([key[:fields]].flatten.count != [key[:reference][:fields]].flatten.count)
-      end
+    def field_count_mismatch?(key)
+      key[:reference] && ([key[:fields]].flatten.count != [key[:reference][:fields]].flatten.count)
+    end
 
-      def add_error(error)
-        @errors << error
-      end
+    def add_error(error)
+      @errors << error
+    end
 
   end
 end
--- spec/validate_spec.rb
+++ spec/validate_spec.rb
@@ -9,13 +9,15 @@ describe TableSchema::Validate do
       it 'with a simple schema' do
         descriptor = load_descriptor('schema_valid_simple.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(true)
+        expect(schema.validate).to eq(true)
+        expect(schema.validate!).to eq(true)
       end
 
       it 'with a full schema' do
         descriptor = load_descriptor('schema_valid_full.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(true)
+        expect(schema.validate).to eq(true)
+        expect(schema.validate!).to eq(true)
       end
 
     end
@@ -25,7 +27,8 @@ describe TableSchema::Validate do
       it 'with an empty schema' do
         descriptor = load_descriptor('schema_invalid_empty.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
+        expect{ schema.validate! }.to raise_error(TableSchema::SchemaException)
       end
 
     end
@@ -39,13 +42,13 @@ describe TableSchema::Validate do
       it 'with a primary key as a string' do
         descriptor = load_descriptor('schema_valid_pk_string.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(true)
+        expect(schema.validate).to eq(true)
       end
 
       it 'with a primary key as an array' do
         descriptor = load_descriptor('schema_valid_pk_array.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(true)
+        expect(schema.validate).to eq(true)
       end
 
     end
@@ -55,25 +58,25 @@ describe TableSchema::Validate do
       it 'when primary key string value is wrong' do
         descriptor = load_descriptor('schema_invalid_pk_string.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'when primary key array value is wrong' do
         descriptor = load_descriptor('schema_invalid_pk_array.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'when the primary key is not a valid type' do
         descriptor = load_descriptor('schema_invalid_pk_is_wrong_type.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'when the schema has no fields' do
         descriptor = load_descriptor('schema_invalid_pk_no_fields.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
         expect(schema.errors.count).to eq(3)
       end
 
@@ -88,19 +91,19 @@ describe TableSchema::Validate do
       it 'with a valid foreign key string' do
         descriptor = load_descriptor('schema_valid_fk_string.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(true)
+        expect(schema.validate).to eq(true)
       end
 
       it 'with a valid foreign key self reference' do
         descriptor = load_descriptor('schema_valid_fk_string_self_referencing.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(true)
+        expect(schema.validate).to eq(true)
       end
 
       it 'with a valid foreign key array' do
         descriptor = load_descriptor('schema_valid_fk_array.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(true)
+        expect(schema.validate).to eq(true)
       end
 
     end
@@ -110,37 +113,37 @@ describe TableSchema::Validate do
       it 'with an invalid foreign key string' do
         descriptor = load_descriptor('schema_invalid_fk_string.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'with a foreign key with no reference' do
         descriptor = load_descriptor('schema_invalid_fk_no_reference.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'with an invalid foreign key array' do
         descriptor = load_descriptor('schema_invalid_fk_array.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'where foreign key reference is an array and fields are a string' do
         descriptor = load_descriptor('schema_invalid_fk_string_array_ref.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'where foreign key reference is an string and fields are an array' do
         descriptor = load_descriptor('schema_invalid_fk_string_array_ref.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
       it 'where there is a foreign key reference and field count mismatch' do
         descriptor = load_descriptor('schema_invalid_fk_array_wrong_number.json')
         schema = TableSchema::Schema.new(descriptor)
-        expect(schema.valid?).to eq(false)
+        expect(schema.validate).to eq(false)
       end
 
     end
